373b4916d3a2d142d3c24884aea1a9d7a1558e1a
PozuTuv1337 <doobl.xz.kakoy@gmail.com>
2025-03-19 10:29:59 +0300

    Изменён файл core_notes.py для имитации порчи данных

--- core_notes.py
+++ core_notes.py
@@ -1,14 +1,7 @@
 # core_notes.py
 # Модуль управления пользователями и создания/редактирования заметок
  
-def register_user(username, password):
-    print(f"User {username} registered successfully.")
- 
-def create_note(note_title, note_content):
-    print(f"Note '{note_title}' created with content: {note_content}")
- 
-def group_notes_into_book(book_title, notes):
-    print(f"Book '{book_title}' created with notes: {notes}")
+#Удалил основные функции для создания необратимых изменений
  
 if __name__ == '__main__':
     register_user("testuser", "password123")
